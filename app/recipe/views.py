8bfadb6a022ced4f3f139baea3a99d0936adbd47
salahgafoor <salahgafoor@gmail.com>
2021-03-14 20:11:53 +0530

    ingredient api finished,errors are present

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag  # , Ingredient, Recipe
+from core.models import Tag, Ingredient  # , Recipe
 
 from recipe import serializers
 
@@ -48,6 +48,23 @@ class TagViewSet(viewsets.GenericViewSet,
 
     def perform_create(self, serializer):
         """Create new tag"""
+        serializer.save(user=self.request.user)
+
+class IngredientViewSet(viewsets.GenericViewSet,
+                        mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
+    """Manage ingredients in the database"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create new ingredients"""
         serializer.save(user=self.request.user)
 
 # class IngredientViewSet(BaseRecipeAttrViewSet):
