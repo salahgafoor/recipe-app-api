e3ee2eceb8d87ab0316602380b7ad0cc20ced192
salahgafoor <salahgafoor@gmail.com>
2021-03-14 15:17:11 +0530

    created user token and tests

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -26,29 +26,29 @@ class UserSerializer(serializers.ModelSerializer):
 #             user.save()
 #
 #         return user
-#
-#
-# class AuthTokenSerializer(serializers.Serializer):
-#     """Serializer for the user authentication object"""
-#     email = serializers.CharField()
-#     password = serializers.CharField(
-#         style={'input_type': 'password'},
-#         trim_whitespace=False
-#     )
-#
-#     def validate(self, attrs):
-#         """Validate and authenticate the user"""
-#         email = attrs.get('email')
-#         password = attrs.get('password')
-#
-#         user = authenticate(
-#             request=self.context.get('request'),
-#             username=email,
-#             password=password
-#         )
-#         if not user:
-#             msg = _('Unable to authenticate with provided credentials')
-#             raise serializers.ValidationError(msg, code='authentication')
-#
-#         attrs['user'] = user
-#         return attrs
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user authentication object"""
+    email = serializers.CharField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        """Validate and authenticate the user"""
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password
+        )
+        if not user:
+            msg = _('Unable to authenticate with provided credentials')
+            raise serializers.ValidationError(msg, code='authentication')
+
+        attrs['user'] = user
+        return attrs
