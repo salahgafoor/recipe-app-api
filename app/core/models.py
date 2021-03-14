d438d55ef52561445232bc28de7d78869dc6e043
salahgafoor <salahgafoor@gmail.com>
2021-03-14 18:55:58 +0530

    recipe app created, tag model created

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,9 @@
+# import uuid
+# import os
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-    PermissionsMixin
+                                        PermissionsMixin
+from django.conf import settings
 
 
 class UserManager(BaseUserManager):
@@ -37,3 +40,45 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Tag(models.Model):
+    """Tag to be used for a recipe"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+# class Ingredient(models.Model):
+#     """Ingredient to be used in a recipe"""
+#     name = models.CharField(max_length=255)
+#     user = models.ForeignKey(
+#         settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE
+#     )
+#
+#     def __str__(self):
+#         return self.name
+#
+#
+# class Recipe(models.Model):
+#     """Recipe object"""
+#     user = models.ForeignKey(
+#         settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE
+#     )
+#     title = models.CharField(max_length=255)
+#     time_minutes = models.IntegerField()
+#     price = models.DecimalField(max_digits=5, decimal_places=2)
+#     link = models.CharField(max_length=255, blank=True)
+#     ingredients = models.ManyToManyField('Ingredient')
+#     tags = models.ManyToManyField('Tag')
+#     image = models.ImageField(null=True, upload_to=recipe_image_file_path)
+#
+#     def __str__(self):
+#         return self.title
